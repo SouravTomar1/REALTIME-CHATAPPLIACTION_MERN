7b410785e1f1ab73fcf4a1efc66dbcb83337d47e
SouravTomar1 <isourav2710@gmail.com>
2025-09-28 15:25:54 +0530

    express version

--- backend/src/libs/utils.js
+++ backend/src/libs/utils.js
@@ -1,14 +1,16 @@
-import jwt from 'jsonwebtoken';
-export const generateToken=(userID,res)=> {
-      const token = jwt.sign({userID},process.env.JWT_SECRET,{
-        expiresIn: "7d"
-})
-res.cookie("jwt",token,{
-    maxAge: 7*24*60*60*1000,  //in milisecond
-    httpOnly: true,//this can make cookie not accesible by js like document.cookkie so this is secure
-    sameSite:"strict", // only send request in same origin or first-partyycontext
-    secure:process.env.NODE_ENV !="development"
-    // CSRF Cross-Site Request Forgery. means if we login in abnking site and do not logout and go to some third part website then that site can acces the jwt token of the banking site
-});
-return token;
-}
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d"
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7*24*60*60*1000, // 7 days
+    httpOnly: true,           // can't be accessed by JS
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "strict",
+    secure: process.env.NODE_ENV === "production"
+  });
+
+  return token;
+};
