743d0d0ada0d925ce1c4d1894b521e0770dbe3cd
SouravTomar1 <isourav2710@gmail.com>
2026-04-12 15:49:36 +0530

    Added Docker support to chat application

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -79,7 +79,7 @@ const ProfilePage = () => {
                 <Mail className="w-4 h-4" />
                 Email Address
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg bordṇer">{authUser?.email}</p>
             </div>
           </div>
 
