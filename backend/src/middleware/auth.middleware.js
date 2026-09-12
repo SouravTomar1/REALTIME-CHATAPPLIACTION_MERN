5323c486f0f13735886ace6a315f588c48d4f6e4
SouravTomar1 <isourav2710@gmail.com>
2026-09-12 16:19:30 +0530

    test: verify automated ci/cd trigger

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -12,7 +12,7 @@ export const protectRoute = async (req, res, next) => {
 
     const user = await User.findById(decoded.userId).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
-
+////check if the  file i
     req.user = user;
     next();
   } catch (error) {
